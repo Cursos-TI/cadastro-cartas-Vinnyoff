98ef9cf4c18b487069b534d0ab172ee420a32815
Marcos V. de Freitas <108553960+Vinnyoff@users.noreply.github.com>
2025-11-15 02:17:36 +0000

    initial commit

---
+++ super_trunfo.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // Definição das variáveis para armazenar as propriedades das cidades
+
+    char estado1;
+    char codigo1[4];
+    char nome1[50];
+    int população1 = 0;
+    float area1;
+    float pib1;
+    int npt1 = 0;
+
+    char estado2;
+    char codigo2[4];
+    char nome2[50];
+    int população2;
+    float area2;
+    float pib2;
+    int npt2;
+
+        // Apresentação do programa e entrada de dados
+    printf("Bem vindo ao Super Trunfo dos Estados Brasileiros!\n\n");
+    printf("Ajude-nos a cadastrar as cartas do jogo\n");
+    printf("Vamos começar pela primeira carta:\n\n");
+    printf("Digite o nome da cidade: ");
+    scanf("%s", nome1);
+    printf("Digite a inicial do estado: ");
+    scanf(" %c", &estado1);
+    printf("Digite a população da cidade: ");
+    scanf("%d", &população1);
+    printf("Digite a área da cidade (em km²): ");
+    scanf("%f", &area1);
+    printf("Digite o PIB da cidade (em bilhões R$): ");
+    scanf("%f", &pib1);
+    printf("Digite o número de pontos turísticos da cidade: ");
+    scanf("%d", &npt1);
+    printf("Digite o codigo da carta: (A letra do estado junto com um número de 01 a 04)");
+    scanf("%s", codigo1);
+
+    printf("\nAgora vamos cadastrar a segunda carta:\n\n");
+    printf("Digite o nome da cidade: ");
+    scanf("%s", nome2);
+    printf("Digite a inicial do estado: ");
+    scanf(" %c", &estado2);
+    printf("Digite a população da cidade: ");
+    scanf("%d", &população2);
+    printf("Digite a área da cidade (em km²): ");
+    scanf("%f", &area2);
+    printf("Digite o PIB da cidade (em bilhões R$): ");
+    scanf("%f", &pib2);
+    printf("Digite o número de pontos turísticos da cidade: ");
+    scanf("%d", &npt2);
+    printf("Digite o codigo da carta: (A letra do estado junto com um número de 01 a 04)");
+    scanf("%s", codigo2);
+
+        // Exibição dos dados das cidades cadastradas
+    printf("\nCartas cadastradas com sucesso!\n");
+    printf("Carta 1:\n Estado: %c\n Codigo: %s\n Nome da cidade: %s\n População: %d\n Área: %.2f km²\n PIB: %.2f bilhões de reais\n Número de pontos turísticos: %d\n",
+           estado1, codigo1, nome1, população1, area1, pib1, npt1);
+    printf("Carta 2:\n Estado: %c\n Codigo: %s\n Nome da cidade: %s\n População: %d\n Área: %.2f km²\n PIB: %.2f bilhões de reais\n Número de pontos turísticos: %d\n",
+           estado2, codigo2, nome2, população2, area2, pib2, npt2);
+    return 0;
+}
